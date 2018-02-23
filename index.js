4f877891017488faab47705b0c9cc4ea45f45886
Victoria Ruiz Schulze <vicky.ruiz.schulze@gmail.com>
2018-02-23 00:08:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ function takeANumber(katzDeliLine, name) {
   return katzDeliLine.indexOf(name) + 1;
 }
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length == 0) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
     return katzDeliLine.shift();
   }
 }
+function currentLine() {}
