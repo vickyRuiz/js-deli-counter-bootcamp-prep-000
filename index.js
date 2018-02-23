42922683e17268b95f8ef8a11fa13b360f76e93a
Victoria Ruiz Schulze <vicky.ruiz.schulze@gmail.com>
2018-02-23 00:15:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line.";
+  
+  return 0;
+  /*return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line.";*/
 }
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
